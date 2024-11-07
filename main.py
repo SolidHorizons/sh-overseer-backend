c144365aaa11b9d0c16c103b2e7d907e894dc8f1
Shrimpey304 <124566452+Shrimpey304@users.noreply.github.com>
2024-11-07 02:07:37 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import discord
+from discord.ext import commands
+import asyncio
+import logging as log
+import datetime as dt
+import os
+from dotenv import load_dotenv, dotenv_values 
+
+
+class main:
+
+    def __init__(self):
+        log_time = dt.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+        current_dir = os.path.dirname(__file__)
+        log_file_path = os.path.join(current_dir, 'applogs', f'Log_{log_time}.txt')
+        log.basicConfig(
+            filename=log_file_path,
+            level=log.INFO,
+            format=f'[%(asctime)s] -- %(message)s',
+            filemode='a'
+        )
+        log.raiseExceptions = False
+        log.captureWarnings(False)
+        load_dotenv()
+
+
+    intents = discord.Intents.all()                            #"applicationID" / "betaApplicationID"
+    bot = commands.Bot(command_prefix='*', intents=intents, application_id=os.getenv("applicationID"))
+
+
+    def run_bot_loop(self) -> None:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        loop.run_until_complete(self.run_bot())
+
+
+    async def run_bot(self) -> None:
+
+        await self.initiateCogs()
+                            #"dcKey" / "betaDcKey"
+        await self.bot.start(os.getenv("dcKey")) # start the bot
+
+
+    async def initiateCogs(self):
+        try:
+            await self.bot.load_extension("maincog")
+
+        except Exception as e:
+            print(f"Failed to load extensions: {e}")
+
+
+
+if __name__ == "__main__":
+
+    instMain = main()
+    bot_thread = instMain.run_bot_loop()
+
---
+++ maincog.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+import os
+import logging as log
+import datetime as dt
+import asyncio
+import json
+import discord.ext.commands
+import random
+import math
+import re
+import discord.ext
+
+async def setup(bot: commands.Bot):
+                                                                #SH ID
+    await bot.add_cog(maincog(bot), guilds=[discord.Object(id=1259185634334081084)])
+
+class maincog(commands.Cog):
+
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_ready(self) -> None:       # when the cog is loaded in it will print in the console that it's working
+        print(f'{self.__cog_name__} is ready')
+
+
+    @commands.command()
+    async def sync(self, ctx : discord.ext.commands.Context) -> None:
+        print("in sync")
+        bot : discord.ext.commands.Bot = ctx.bot
+        fmt = await bot.tree.sync(guild=ctx.guild) 
+        await ctx.send(f"synced {len(fmt)} commands")
+
+
+    @app_commands.command(name="sayhello", description="say hello bot")
+    async def sayhello(self, interaction: discord.Interaction):
+        await interaction.response.send_message("hello")
